331348753cf51c82db846f56f26c47ad27804ea5
in4margaret <in4margaret@gmail.com>
2018-08-25 20:08:34 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const PORT = 8080;
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log(`Running http://localhost:${PORT}`);
